0c7e7266457075f1ceec99abf0098a09ede0fad8
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-02-18 21:06:24 -0300

    Implementa appBar no about.dart

--- lib/about.dart
+++ lib/about.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Welcome to About Screen"),
+    return Scaffold(
+        appBar: AppBar(
+          foregroundColor: Colors.black,
+          title: const Text("About Screen", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+        ),
+        body: const Center(
+          child: Text("Welcome to About Screen"),
+        ),
     );
   }
 }
